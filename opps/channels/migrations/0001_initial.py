71355c039b1ca97a3c431d63aa8d18588a1d90a6
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-09-11 01:38:51 -0300

    generate initial migratin

--- opps/channels/migrations/0001_initial.py
+++ opps/channels/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import django.utils.timezone
+import mptt.fields
 from django.conf import settings
 
 
@@ -25,11 +26,8 @@ class Migration(migrations.Migration):
                 ('date_available', models.DateTimeField(default=django.utils.timezone.now, null=True, verbose_name='Date available', db_index=True)),
                 ('published', models.BooleanField(default=False, db_index=True, verbose_name='Published')),
                 ('slug', models.SlugField(max_length=150, verbose_name='Slug')),
-                ('path', models.CharField(unique=True, max_length=255)),
-                ('depth', models.PositiveIntegerField()),
-                ('numchild', models.PositiveIntegerField(default=0)),
                 ('name', models.CharField(max_length=60, verbose_name='Name')),
-                ('long_slug', models.SlugField(max_length=250, verbose_name='Path name')),
+                ('long_slug', models.CharField(db_index=True, max_length=250, null=True, verbose_name='Long slug', blank=True)),
                 ('layout', models.CharField(default=b'default', max_length=250, verbose_name='Layout', db_index=True)),
                 ('description', models.CharField(max_length=255, null=True, verbose_name='Description', blank=True)),
                 ('hat', models.CharField(max_length=255, null=True, verbose_name='Hat', blank=True)),
@@ -37,8 +35,13 @@ class Migration(migrations.Migration):
                 ('include_in_main_rss', models.BooleanField(default=True, verbose_name='Show in main RSS?')),
                 ('homepage', models.BooleanField(default=False, help_text='Check only if this channel is the homepage. Should have only one homepage per site', verbose_name='Is home page?')),
                 ('group', models.BooleanField(default=False, verbose_name='Group sub-channel?')),
-                ('order', models.IntegerField(default=0, verbose_name='Order')),
+                ('order', models.PositiveIntegerField(default=0, verbose_name='Order')),
                 ('paginate_by', models.IntegerField(null=True, verbose_name='Paginate by', blank=True)),
+                ('lft', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('level', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('parent', mptt.fields.TreeForeignKey(related_name=b'subchannel', verbose_name='Parent', blank=True, to='channels.Channel', null=True)),
                 ('site', models.ForeignKey(default=1, to='sites.Site')),
                 ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
             ],
