b8f09714accaa1bc4c1dd8f4a971ee8b11c4fe97
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-09-18 00:36:39 -0300

    used parser argument on exportcontainerbox_314

--- opps/bin/opps-admin.py
+++ opps/bin/opps-admin.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Opps CMS bin file')
     parser.add_argument('operation', help='task to be performed',
                         choices=['startproject', 'exportcontainer'])
+    parser.add_argument("project_name", help="Project name", type=str)
 
     args = parser.parse_args()
     if args.operation == 'startproject':
-        parser.add_argument("project_name", help="Project name", type=str)
         management.call_command(
             'startproject', args.project_name,
             template='https://github.com/opps/opps-project-template/zipball/'
@@ -23,8 +23,7 @@ if __name__ == "__main__":
         )
 
     elif args.operation == 'exportcontainer':
-        parser.add_argument("settings", help="Project settings", type=str)
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", args.settings)
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", args.project_name)
 
         from django.core import serializers
         from opps.containers.models import ContainerBox
