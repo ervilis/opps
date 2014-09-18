aef1b4a8276a156c11c9c276d1d3f180b8d54675
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-09-18 00:21:23 -0300

    import settings after set DJANGO_SETTINGS_MODULE

--- opps/bin/opps-admin.py
+++ opps/bin/opps-admin.py
@@ -3,7 +3,7 @@
 import os
 import argparse
 from django.conf import settings
-from django.core import management, serializers
+from django.core import management
 
 
 settings.configure()
@@ -26,6 +26,7 @@ if __name__ == "__main__":
         parser.add_argument("settings", help="Project settings", type=str)
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", args.settings)
 
+        from django.core import serializers
         from opps.containers.models import ContainerBox
         from opps.boxes.models import QuerySet
         from opps.channels.models import Channel
