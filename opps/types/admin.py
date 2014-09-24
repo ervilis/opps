bf549fae94bcf1fec7c823bcb820df6b87f9705a
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-09-24 20:08:12 -0300

    create basic admin opps.types (post, album and link)

--- opps/types/admin.py
+++ opps/types/admin.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Album, Link
 
 
 class PostAdmin(admin.ModelAdmin):
     pass
 
 
+class AlbumAdmin(admin.ModelAdmin):
+    pass
+
+
+class LinkAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Link, LinkAdmin)
 admin.site.register(Post, PostAdmin)
