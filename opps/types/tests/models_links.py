9fb97c945d95512624bc002d99feea77b6397c66
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-10-02 20:46:40 -0300

    fix typo on link model

--- opps/types/tests/models_links.py
+++ opps/types/tests/models_links.py
@@ -3,12 +3,12 @@ from django.test import TestCase
 
 from model_mommy import mommy
 
-from ..models import Links
+from ..models import Link
 
 
 class ImageTest(TestCase):
     def setUp(self):
-        self.link = mommy.make(Links)
+        self.link = mommy.make(Link)
 
     def test_exist(self):
         self.assertTrue(self.link)
