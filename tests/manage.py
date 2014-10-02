224f5f4d08a452abae75fb5adcf97825f456e1c7
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-10-01 22:06:40 -0300

    append pull path on python path

--- tests/manage.py
+++ tests/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    sys.path.append('tests')
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    sys.path.append(os.path.join(BASE_DIR, 'tests'))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
 
     from django.core.management import execute_from_command_line
