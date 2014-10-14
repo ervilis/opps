d06b03ee6e9c481dc7aadf8133b1d6576e9c28c5
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-10-14 16:25:58 -0300

    update container migration 0001

--- opps/containers/migrations/0001_initial.py
+++ opps/containers/migrations/0001_initial.py
@@ -20,6 +20,7 @@ class Migration(SchemaMigration):
             ('site_domain', self.gf('django.db.models.fields.CharField')(db_index=True, max_length=100, null=True, blank=True)),
             ('date_available', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, null=True, db_index=True)),
             ('published', self.gf('django.db.models.fields.BooleanField')(default=False, db_index=True)),
+            ('show_on_root_channel', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('uid', self.gf('django.db.models.fields.CharField')(db_index=True, max_length=60, null=True, blank=True)),
             ('child_class', self.gf('django.db.models.fields.CharField')(db_index=True, max_length=30, null=True, blank=True)),
             ('child_module', self.gf('django.db.models.fields.CharField')(db_index=True, max_length=120, null=True, blank=True)),
@@ -39,6 +40,40 @@ class Migration(SchemaMigration):
         ))
         db.create_unique(m2m_table_name, ['container_id', 'channel_id'])
 
+        # Adding model 'QuerySet'
+        db.create_table(u'containers_queryset', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('date_insert', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('date_update', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
+            ('site', self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['sites.Site'])),
+            ('site_uid', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True, max_length=4, null=True, blank=True)),
+            ('site_domain', self.gf('django.db.models.fields.CharField')(db_index=True, max_length=100, null=True, blank=True)),
+            ('date_available', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, null=True, db_index=True)),
+            ('published', self.gf('django.db.models.fields.BooleanField')(default=False, db_index=True)),
+            ('show_on_root_channel', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=150)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=140)),
+            ('model', self.gf('django.db.models.fields.CharField')(max_length=150)),
+            ('limit', self.gf('django.db.models.fields.PositiveIntegerField')(default=7)),
+            ('offset', self.gf('django.db.models.fields.PositiveIntegerField')(default=0)),
+            ('order_field', self.gf('django.db.models.fields.CharField')(default=u'id', max_length=100)),
+            ('order', self.gf('django.db.models.fields.CharField')(max_length=1)),
+            ('recursive', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('filters', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('excludes', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal(u'containers', ['QuerySet'])
+
+        # Adding M2M table for field channel on 'QuerySet'
+        m2m_table_name = db.shorten_name(u'containers_queryset_channel')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('queryset', models.ForeignKey(orm[u'containers.queryset'], null=False)),
+            ('channel', models.ForeignKey(orm[u'channels.channel'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['queryset_id', 'channel_id'])
+
 
     def backwards(self, orm):
         # Removing unique constraint on 'Container', fields ['site', 'uid']
@@ -49,6 +84,12 @@ class Migration(SchemaMigration):
 
         # Removing M2M table for field channel on 'Container'
         db.delete_table(db.shorten_name(u'containers_container_channel'))
+
+        # Deleting model 'QuerySet'
+        db.delete_table(u'containers_queryset')
+
+        # Removing M2M table for field channel on 'QuerySet'
+        db.delete_table(db.shorten_name(u'containers_queryset_channel'))
 
 
     models = {
@@ -122,10 +163,35 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'polymorphic_ctype': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'polymorphic_containers.container_set'", 'null': 'True', 'to': u"orm['contenttypes.ContentType']"}),
             'published': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
+            'show_on_root_channel': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'site': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': u"orm['sites.Site']"}),
             'site_domain': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'site_uid': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True', 'max_length': '4', 'null': 'True', 'blank': 'True'}),
             'uid': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '60', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        u'containers.queryset': {
+            'Meta': {'object_name': 'QuerySet'},
+            'channel': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "u'channeling_containers_queryset_sets+'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['channels.Channel']"}),
+            'date_available': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'db_index': 'True'}),
+            'date_insert': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_update': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'excludes': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'filters': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'limit': ('django.db.models.fields.PositiveIntegerField', [], {'default': '7'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '140'}),
+            'offset': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'order': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'order_field': ('django.db.models.fields.CharField', [], {'default': "u'id'", 'max_length': '100'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
+            'recursive': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'show_on_root_channel': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': u"orm['sites.Site']"}),
+            'site_domain': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'site_uid': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True', 'max_length': '4', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '150'}),
             'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'blank': 'True'})
         },
         u'contenttypes.contenttype': {
