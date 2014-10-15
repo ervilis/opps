e25e2edf02d124af9920f73bcb38bae8dc977eb4
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-10-15 13:36:33 -0300

    refactoring model choice, get all package opps.containers.types

--- opps/core/forms.py
+++ opps/core/forms.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from .conf import settings
 
 
 def model_choices():
     try:
-        return tuple(sorted([
+        m = tuple(sorted([
             (u"{0}.{1}".format(app._meta.app_label, app._meta.object_name),
              u"{0} - {1}".format(app._meta.app_label, app._meta.object_name))
-            for app in models.get_models() if 'opps.' in app.__module__]))
+            for app in models.get_models()
+            if 'opps.containers.type' in app.__module__]))
+
+        return tuple(sorted(m + settings.OPPS_CONTAINER_TYPES))
     except ImportError:
         return tuple([])
