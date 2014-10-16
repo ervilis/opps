2c16720945e26cc3c1969348bc0b8b1c0d816036
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-10-16 14:53:15 -0300

    maker container conf

--- opps/containers/conf.py
+++ opps/containers/conf.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from appconf import AppConf
+
 from .types.images.conf import ThumborConf
+
+
+class OppsContainerConf(AppConf):
+
+    CACHE_EXPIRE_LIST = 5
+    PAGINATE_BY = 10
+    VIEWS_LIMIT = 10
+    CONTAINER_TYPES = ()
+
+    class Meta:
+        prefix = 'opps'
