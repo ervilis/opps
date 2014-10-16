a2b1776c099c7a119bc4531838a2a7cc27ccf940
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-10-16 15:04:15 -0300

    opps url

---
+++ opps/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('opps.containers.urls', namespace='containers',
+                      app_name='containers')),
+)
